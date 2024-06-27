f00e0306227df99c62a45e8264f9c067ef8276a9
daniel de brito <danieldebrito@outlook.com>
2024-06-27 19:13:27 -0300

    login

--- src/app/auth/pages/sign-in/sign-in.component.ts
+++ src/app/auth/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ERole, Usuario } from '../../class/usuario';
@@ -10,10 +10,12 @@ import { Router } from '@angular/router';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
   public error = false;
   public mostrarPass = false;
+  public usuariosAccesoRapido: Usuario[] = [];
+
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -25,8 +27,11 @@ export class SignInComponent {
 
   constructor(
     private authSvc: AuthService,
+    public usuariosSv: UsuariosService,
     private router: Router,
   ) {}
+
+  
 
   async onLogin(): Promise<void> {
     const { email, password } = this.loginForm.value;
@@ -51,4 +56,47 @@ export class SignInComponent {
     
     //this.authSvc.SignIn( { email: 'danielrdebrito@gmail.com', password: '123456'});
   }
+
+  public login(email: string, password: string){
+    let user: Usuario = {
+      email: email,
+      password: password
+    }
+    this.authSvc.SignIn(user);
+  }
+
+  public loguear(item: any){
+
+    let user: Usuario = {
+      email: item.email,
+      password: item.password
+    }
+
+    this.loginForm.setValue({
+      email: item.email,
+      password: item.password
+    });
+
+    //this.authSvc.SignIn(user);
+
+  }
+
+  public accesosRapidos(){
+    this.usuariosSv.getItems().subscribe( usuarios => {
+
+      const pacientes: Usuario[] = usuarios.filter(usuario => usuario.role === ERole.paciente).slice(0, 3);
+      const especialistas: Usuario[] = usuarios.filter(usuario => usuario.role === ERole.especialista).slice(0, 2);
+      const admin: Usuario[] = usuarios.filter(usuario => usuario.role === ERole.administrador).slice(0, 1);
+
+      this.usuariosAccesoRapido = pacientes.concat(especialistas.concat(admin));
+      
+      //console.log(this.usuariosAccesoRapido);
+
+    } );
+  }
+
+  ngOnInit() { 
+    this.accesosRapidos();
+  }
 }
+
