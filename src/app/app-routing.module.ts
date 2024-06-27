4bf1b72d853a7d698844ce92f76d77d68c5eb752
daniel de brito <danieldebrito@outlook.com>
2024-06-27 19:32:23 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,16 +17,13 @@ const routes: Routes = [
   { path: 'signup-selector', loadChildren: () => import('./auth/pages/sign-up-selector/sign-up-selector.module').then(m => m.SignUpSelectorModule) },
     // pages ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'administrador', loadChildren: () => import('./pages/admin/administrador.module').then(m => m.AdministradorModule) },
   { path: 'denegado', loadChildren: () => import('./pages/denegado/denegado.module').then(m => m.DenegadoModule) },
-  { path: 'jornadas', loadChildren: () => import('./pages/especialistas/pages/jornada/jornada.module').then(m => m.JornadaModule), /* canActivate: [EspecialistaGuard] */ },
 
-
-
-  //  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  { path: 'administrador', loadChildren: () => import('./pages/admin/administrador.module').then(m => m.AdministradorModule), canActivate: [AdminGuard] },
+  { path: 'jornadas', loadChildren: () => import('./pages/especialistas/pages/jornada/jornada.module').then(m => m.JornadaModule), canActivate: [EspecialistaGuard] },
   { path: 'misturnos', loadChildren: () => import('./pages/turnos/pages/mis-turnos/mis-turnos.module').then(m => m.MisTurnosModule)},
-  { path: 'sacarturno', loadChildren: () => import('./pages/turnos/pages/sacar-turno/sacar-turno.module').then(m => m.SacarTurnoModule)/* , canActivate: [PacienteGuard] */ },
-  { path: 'especialidades', loadChildren: () => import('./pages/especialidades/especialidades.module').then(m => m.EspecialidadesModule), /* canActivate: [AdminGuard] */ },
+  { path: 'sacarturno', loadChildren: () => import('./pages/turnos/pages/sacar-turno/sacar-turno.module').then(m => m.SacarTurnoModule), canActivate: [PacienteGuard] },
+  { path: 'especialidades', loadChildren: () => import('./pages/especialidades/especialidades.module').then(m => m.EspecialidadesModule), canActivate: [AdminGuard] },
   { path: 'atenderturno', loadChildren: () => import('./pages/especialistas/pages/atender-turno/atender-turno.module').then(m => m.AtenderTurnoModule) },
   { path: 'mispacientes', loadChildren: () => import('./pages/especialistas/pages/mis-pacientes/mis-pacientes.module').then(m => m.MisPacientesModule) },
     //{ path: 'historiaclinica', loadChildren: () => import('./pages/historia-clinica/historia-clinica.module').then(m => m.HistoriaClinicaModule) },
