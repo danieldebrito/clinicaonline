f23a99479534e620057b044fcd8680756220f988
daniel de brito <danieldebrito@outlook.com>
2024-07-01 00:52:28 -0300

    perfeil especialista

--- src/app/pages/especialistas/components/especialista-ficha/especialista-ficha.component.ts
+++ src/app/pages/especialistas/components/especialista-ficha/especialista-ficha.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
 import { Especialista } from '../../../../class/usuarios/especialista';
 
 @Component({
@@ -7,17 +6,9 @@ import { Especialista } from '../../../../class/usuarios/especialista';
   templateUrl: './especialista-ficha.component.html',
   styleUrl: './especialista-ficha.component.scss'
 })
-export class EspecialistaFichaComponent implements OnInit {
+export class EspecialistaFichaComponent {
   
-  @Input() especialista: Especialista | undefined;
+  @Input() especialista: Especialista | undefined = {};
 
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    // Lógica adicional si es necesario
-  }
-
-  volver(): void {
-    this.router.navigate(['/previous-page']);  // Cambia '/previous-page' a la ruta correspondiente
-  }
 }
