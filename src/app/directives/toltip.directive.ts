972163a3252f8e7447065e208b6758f7e2d1ecfd
Daniel de Brito <danieldebrito@outlook.comclear>
2024-07-10 12:56:29 -0300

    tooltip directiva

--- src/app/directives/toltip.directive.ts
+++ src/app/directives/toltip.directive.ts
@@ -1,59 +1,22 @@
-import { Directive, ElementRef, Input, Renderer2, HostListener, OnInit, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appTooltip]',
+  selector: '[appTooltip]'
 })
-export class TooltipDirective implements OnInit, OnDestroy {
-  @Input('appTooltip') tooltipTitle: string = '';
-  tooltip: HTMLElement | any = null;
+export class TooltipDirective {
+  @Input('appTooltip') tooltipText: string = '';
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private el: ElementRef) {}
 
-  ngOnInit() {
-    this.renderer.setStyle(this.el.nativeElement, 'position', 'relative');
+  @HostListener('mouseenter')
+  showTooltip() {
+    // Muestra el tooltip al pasar el mouse por encima
+    this.el.nativeElement.title = this.tooltipText;
   }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    if (!this.tooltip) {
-      this.showTooltip();
-    }
-  }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    if (this.tooltip) {
-      this.hideTooltip();
-    }
-  }
-
-  private showTooltip() {
-    this.tooltip = this.renderer.createElement('span');
-    this.renderer.appendChild(this.tooltip, this.renderer.createText(this.tooltipTitle));
-    this.renderer.appendChild(this.el.nativeElement, this.tooltip);
-    this.renderer.addClass(this.tooltip, 'tooltip');
-
-    const hostPos = this.el.nativeElement.getBoundingClientRect();
-    const tooltipPos = this.tooltip.getBoundingClientRect();
-
-    const top = hostPos.top - tooltipPos.height -
-      (this.el.nativeElement.style.paddingTop || 0) +
-      (this.el.nativeElement.style.borderTopWidth || 0);
-    const left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
-
-    this.renderer.setStyle(this.tooltip, 'top', `${top}px`);
-    this.renderer.setStyle(this.tooltip, 'left', `${left}px`);
-  }
-
-  private hideTooltip() {
-    if (this.tooltip) {
-      this.renderer.removeChild(this.el.nativeElement, this.tooltip);
-      this.tooltip = null;
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.tooltip) {
-      this.renderer.removeChild(this.el.nativeElement, this.tooltip);
-      this.tooltip = null;
-    }
+  @HostListener('mouseleave')
+  hideTooltip() {
+    // Oculta el tooltip al quitar el mouse
+    this.el.nativeElement.title = '';
   }
 }
